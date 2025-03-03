c9bc3a185d383ffd1c7fc5ae09cdc960d3e777fc
Arthur <cool.arthur7@gmail.com>
2025-03-03 11:02:26 +0200

    upd

--- dist/types/types-utils.d.ts
+++ dist/types/types-utils.d.ts
@@ -2,5 +2,5 @@ export declare function snakeToHuman(str: string): string;
 export declare function ColumnsFromModel(modelName: string, fields: string[]): {
     header: string;
     path: string;
-    type: import("./types.decorator").Modifier;
+    type: import("./types.decorator").Modifier | "enum";
 }[];
